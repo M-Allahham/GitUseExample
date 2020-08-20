53d169c580c9697b02a2026198b75fbd9100b310
Malik Allahham <allahham.malik@gmail.com>
2020-08-20 10:13:37 -0700

    Intial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Malik Allahham on 8/20/20.
+//  Copyright © 2020 Malik Allahham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
